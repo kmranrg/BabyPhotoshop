b127883dbc1238a462eacd03072a7667421f06b3
kmranrg <kmranrg@gmail.com>
2020-11-06 19:22:21 +0530

    added image uploading feature

--- photoshop/views.py
+++ photoshop/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def photo(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['imageFile']
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
     return render(request, 'photo.html')
