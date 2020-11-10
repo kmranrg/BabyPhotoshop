ceb9e739d211540644a56a058ef9d0190d81be75
kmranrg <kmranrg@gmail.com>
2020-11-11 00:09:16 +0530

    added contrast feature

--- photoshop/urls.py
+++ photoshop/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('photo_sharpness/', views.photo_sharpness, name='photo_sharpness'),
     path('photo_saturation/', views.photo_saturation, name='photo_saturation'),
     path('photo_transpose/', views.photo_transpose, name='photo_transpose'),
-    path('photo_brightness/', views.photo_brightness, name='photo_brightness')
+    path('photo_brightness/', views.photo_brightness, name='photo_brightness'),
+    path('photo_contrast/', views.photo_contrast, name='photo_contrast')
 ]
